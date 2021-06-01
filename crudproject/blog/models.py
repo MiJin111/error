6a7b79575a5eff37f47e9e46f69be58de17ac87d
MiJin111 <wnalwls201@naver.com>
2021-06-01 21:16:43 +0900

    aa

--- crudproject/blog/models.py
+++ crudproject/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model): # models.Model 상속
+    title = models.CharField(max_length=100)
+    writer = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
